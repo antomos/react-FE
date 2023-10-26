a5d8fb86893030f047458e0242a604c9f4dd35e4
antonio moschetti <antoniomoschetti@hotmail.it>
2023-10-26 14:08:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Footer, Blog, Features, Possibility, WhatGPT3, Header} from './containers';
+import { Article, Brand, CTA, Feature, Navbar} from './components';
+import './App.css'
+import './index.css'
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+      <Navbar />
+      <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+
+    </div>
+
+
+  )
+}
+
+export default App
